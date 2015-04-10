fa1a92ab71a58aa76bd88821774441edc9d7d3bf
muratarslan <murat.arslan@neu.edu.tr>
2015-04-10 19:45:01 +0300

    search terms modified for dbpedia

--- searchengine/views.py
+++ searchengine/views.py
@@ -1,37 +1,40 @@
 from django.shortcuts import render
 from django.http import Http404, HttpResponse, HttpResponseRedirect
 from src import *
+import re
 from django.views.decorators.csrf import csrf_exempt
+
 
 # For verification error
 @csrf_exempt
 
+
+
 def search(request):
 	if request.method == 'POST':
 
-		result = goog(request.POST['term'], 3)
+		result = goog(modify(request.POST['term']), 3)
+		rdf_birth   = list(birth(modify(request.POST['term'])))
+		rdf_death = list(death(modify(request.POST['term'])))
+		rdf_desc   = list(description(modify(request.POST['term'])))
+		rdf_thumb = thumbnail(modify(request.POST['term']))
+		rdf_child   = list(children(modify(request.POST['term'])))
 
-		rdf_birth   = list(birth(request.POST['term']))
-		rdf_death = list(death(request.POST['term']))
-		rdf_desc   = list(description(request.POST['term']))
-		rdf_thumb = thumbnail(request.POST['term'])
-		rdf_child   = list(children(request.POST['term']))
-
-                b = {}
-		d = {}
+                brth = {}
+		dth = {}
 		desc = {}
 		thumb = {}
 		child = {}
 
 		# Birth Date
                 if len(rdf_birth) > 0:
-                    b.update({'link': unicode(rdf_birth[0][0]), 'birth_date': unicode(rdf_birth[0][1])})
-                print b
+                    brth.update({'link': unicode(rdf_birth[0][0]), 'birth_date': unicode(rdf_birth[0][1])})
+                print brth
 
 		# Death Date
 		if len(rdf_death) > 0:
-                    d.update({'link': unicode(rdf_death[0][0]), 'death_date': unicode(rdf_death[0][1])})
-                print d
+                    dth.update({'link': unicode(rdf_death[0][0]), 'death_date': unicode(rdf_death[0][1])})
+                print dth
 
 		# Description
 		if len(rdf_desc) > 0:
@@ -51,12 +54,17 @@ def search(request):
 
 
 		return render(request,'search.html', {'result': result, 
-									      'birth_dict': b, 
-									      'death_dict':d,
+									      'birth_dict': brth, 
+									      'death_dict':dth,
 									      'desc_dict':desc,
 									      'thumb_dict':thumb,
 									      'child_dict':child,
 									      'term': request.POST['term']})
 	return render(request,'search.html')
+
+def modify(s):
+     s = re.sub(r"[^\w\s]", '', s) #non-word characters
+     s = re.sub(r"\s+", '_', s)     # replace ' ' to '_'
+     return s.title()
 
 
